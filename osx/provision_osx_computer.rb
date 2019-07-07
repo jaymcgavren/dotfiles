f9397667651cab73abe151279019769741005bc4
Jay McGavren <jay@mcgavren.com>
2019-07-07 14:03:49 -0700

    Move linking of dotfiles before installation of packages

--- osx/provision_osx_computer.rb
+++ osx/provision_osx_computer.rb
@@ -229,6 +229,15 @@ def run(command)
   puts `#{command} 2>&1`
 end
 
+# Link dotfiles to paths where they'll actually be accessed.
+run "ln -s ~/dotfiles/gitconfig ~/.gitconfig"
+run "ln -s ~/dotfiles/gitignore ~/.gitignore"
+run "ln -s ~/dotfiles/inputrc ~/.inputrc"
+run "ln -s ~/dotfiles/irbrc ~/.irbrc"
+run "ln -s ~/dotfiles/rp5rc ~/.rp5rc"
+run "ln -s ~/dotfiles/slate ~/.slate"
+run "ln -s ~/dotfiles/tmux.conf ~/.tmux.conf"
+
 # Add additional Homebrew sources.
 run "brew tap homebrew/dupes"
 run "brew tap homebrew/games"
@@ -255,15 +264,6 @@ run "brew linkapps"
 run "brew cleanup"
 run "brew prune"
 run "brew cask cleanup"
-
-# Link dotfiles to paths where they'll actually be accessed.
-run "ln -s ~/dotfiles/gitconfig ~/.gitconfig"
-run "ln -s ~/dotfiles/gitignore ~/.gitignore"
-run "ln -s ~/dotfiles/inputrc ~/.inputrc"
-run "ln -s ~/dotfiles/irbrc ~/.irbrc"
-run "ln -s ~/dotfiles/rp5rc ~/.rp5rc"
-run "ln -s ~/dotfiles/slate ~/.slate"
-run "ln -s ~/dotfiles/tmux.conf ~/.tmux.conf"
 
 # Eliminate useless fading animation on iTerm2 hotkey window.
 run "defaults write com.googlecode.iterm2 HotkeyTermAnimationDuration -float 0.00001"
