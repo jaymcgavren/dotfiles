74a93be780b47161da0fbffd900497dd961d54c2
Jay McGavren <jay@mcgavren.com>
2024-05-02 13:10:56 -0700

    Move "ln -s ~/dotfiles" commands from osx to OS-agnostic script

---
+++ bootstrap/link_dotfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Link dotfiles to paths where they'll actually be accessed.
+run "ln -s ~/dotfiles/agignore ~/.agignore"
+run "ln -s ~/dotfiles/gitconfig ~/.gitconfig"
+run "ln -s ~/dotfiles/gitignore ~/.gitignore"
+run "ln -s ~/dotfiles/inputrc ~/.inputrc"
+run "ln -s ~/dotfiles/gitconfig ~/.gitconfig"
+run "ln -s ~/dotfiles/gitignore ~/.gitignore"
+run "ln -s ~/dotfiles/irbrc ~/.irbrc"
+run "ln -s ~/dotfiles/pryrc ~/.pryrc"
+run "ln -s ~/dotfiles/rp5rc ~/.rp5rc"
+run "ln -s ~/dotfiles/slate ~/.slate"
+run "ln -s ~/dotfiles/tmux.conf ~/.tmux.conf"
+run "ln -s ~/dotfiles/zprofile ~/.zprofile"
+run "ln -s ~/dotfiles/zshrc ~/.zshrc"
--- osx/provision_osx_computer.rb
+++ osx/provision_osx_computer.rb
@@ -6,21 +6,6 @@ def run(command)
   puts `#{command} 2>&1`
 end
 
-# Link dotfiles to paths where they'll actually be accessed.
-run "ln -s ~/dotfiles/agignore ~/.agignore"
-run "ln -s ~/dotfiles/gitconfig ~/.gitconfig"
-run "ln -s ~/dotfiles/gitignore ~/.gitignore"
-run "ln -s ~/dotfiles/inputrc ~/.inputrc"
-run "ln -s ~/dotfiles/gitconfig ~/.gitconfig"
-run "ln -s ~/dotfiles/gitignore ~/.gitignore"
-run "ln -s ~/dotfiles/irbrc ~/.irbrc"
-run "ln -s ~/dotfiles/pryrc ~/.pryrc"
-run "ln -s ~/dotfiles/rp5rc ~/.rp5rc"
-run "ln -s ~/dotfiles/slate ~/.slate"
-run "ln -s ~/dotfiles/tmux.conf ~/.tmux.conf"
-run "ln -s ~/dotfiles/zprofile ~/.zprofile"
-run "ln -s ~/dotfiles/zshrc ~/.zshrc"
-
 run "brew bundle --file ~/dotfiles/Brewfile"
 
 # Clean up Homebrew cache.
